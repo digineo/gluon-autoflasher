1831c202d1ea68c8e04a2e6d562be60555f9ad11
Julian Kornberger <jk+github@digineo.de>
2014-08-07 14:15:37 +0200

    add missing dash

--- autoflash.sh
+++ autoflash.sh
@@ -16,15 +16,17 @@ function curl_admin() {
 
 # download missing firmware images
 for model in tp-link-tl-wr841n-nd-v8 tp-link-tl-wr841n-nd-v9 tp-link-tl-wdr3500-v1 tp-link-tl-wdr3600-v1 tp-link-tl-wdr4300-v1; do
-	if [ ! -r "images/${base_fw_name}${model}.bin" ]; then
+	filename="${base_fw_name}-${model}.bin"
+	imagefile="images/${filename}"
+	if [ ! -r $imagefile ]; then
 		echo -en "Downloading image for '$model' ... "
-		wget -q "${base_fw_url}${base_fw_name}-${model}.bin" -O "images/${base_fw_name}${model}.bin"
+		wget -q "${base_fw_url}${filename}" -O "$imagefile"
 		if [ $? -eq 0 ]; then
 			echo "OK"
 		else
 			echo "ERROR"
-			rm "images/${base_fw_name}${model}.bin" 2>/dev/null
-			echo "Failed to download firmware. Please ensure the firmware for '${base_fw_name}${model}' is present in images/ directory."
+			rm -f "$imagefile"
+			echo "Failed to download firmware. Please ensure the firmware for '${base_fw_name}-${model}' is present in images/ directory."
 			quit 3
 		fi
 	fi
@@ -36,28 +38,28 @@ if [ $? -ne 0 ]; then
 	quit 1
 fi
 
-model=$(curl_admin http://192.168.0.1/ | grep -oE "WD?R[0-9]+N?")
-echo "found model: $model"
-
 mac=$(arp -i eth0 -a 192.168.0.1 |grep -oE " [0-9a-f:]+ " |tr -d ' ')
 echo "mac address: $mac"
 
+model=$(curl_admin http://192.168.0.1/ | grep -oE "WD?R[0-9]+N?")
+echo "found model: $model"
+
 hwver_page="http://192.168.0.1/userRpm/SoftwareUpgradeRpm.htm"
 hwver=$(curl_admin -e http://192.168.0.1/userRpm/MenuRpm.htm $hwver_page | grep -oE "$model v[0-9]+")
-echo "hw version: $hwver"
+echo "hw version:  $hwver"
 
 uploadurl="http://192.168.0.1/incoming/Firmware.htm"
 image=""
 if [ "$hwver" = "WR841N v9" ]; then
-	image="${base_fw_name}tp-link-tl-wr841n-nd-v9.bin"
+	image="${base_fw_name}-tp-link-tl-wr841n-nd-v9.bin"
 elif [ "$hwver" = "WR841N v8" ]; then
-	image="${base_fw_name}tp-link-tl-wr841n-nd-v8.bin"
+	image="${base_fw_name}-tp-link-tl-wr841n-nd-v8.bin"
 elif [ "$hwver" = "WDR3500 v1" ]; then
-	image="${base_fw_name}tp-link-tl-wdr3500-v1.bin"
+	image="${base_fw_name}-tp-link-tl-wdr3500-v1.bin"
 elif [ "$hwver" = "WDR3600 v1" ]; then
-	image="${base_fw_name}tp-link-tl-wdr3600-v1.bin"
+	image="${base_fw_name}-tp-link-tl-wdr3600-v1.bin"
 elif [ "$hwver" = "WDR4300 v1" ]; then
-	image="${base_fw_name}tp-link-tl-wdr4300-v1.bin"
+	image="${base_fw_name}-tp-link-tl-wdr4300-v1.bin"
 else
 	echo "UNKNOWN MODEL ($hwver), SORRY :("
 	quit 2
