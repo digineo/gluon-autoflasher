d109352ead558053430451638e951d54cdc7d11e
Julian Kornberger <jk+github@digineo.de>
2016-01-05 21:58:39 +0100

    Improve interactive configuration
    Add configurable defaults for meshvpn and bandwidth limiting

--- lib/gluon/setup.py
+++ lib/gluon/setup.py
@@ -8,8 +8,7 @@ from sys import stdin
 from . import *
 
 def all():
-  val = config.get('authorized_keys',"")
-
+  val = getConfig('authorized_keys')
   if val != "":
     with open(val, 'r') as f:
       uploadKeys(f.read())
@@ -28,14 +27,14 @@ def wizard():
     "cbi.submit":                 1,
     "cbi.cbe.wizard.1._meshvpn":  1,
     "cbi.cbe.wizard.1._location": 1,
-    "cbid.wizard.1._contact":     readInput("contact", config.get('contact', defaults['cbid.wizard.1._contact'])),
+    "cbid.wizard.1._contact":     readInput("contact", getConfig('contact', default=defaults['cbid.wizard.1._contact'])),
     "cbid.wizard.1._hostname":    readInput("hostname", defaults['cbid.wizard.1._hostname']),
-    "cbid.wizard.1._meshvpn":     readBool("meshvpn"),
+    "cbid.wizard.1._meshvpn":     readBool("meshvpn", getConfig('meshvpn')),
   }
 
   # Traffic shaping
-  if data["cbid.wizard.1._meshvpn"] == "1":
-    data["cbid.wizard.1._limit_enabled"] = readBool("limit bandwith")
+  if data["cbid.wizard.1._meshvpn"] in [1,"1"]:
+    data["cbid.wizard.1._limit_enabled"] = readBool("limit bandwith", getConfig("limit.enabled"))
 
   # send request
   response = requests.post("http://192.168.1.1/cgi-bin/luci", data=data, headers={"referer": "http://192.168.1.1/cgi-bin/luci"})
@@ -47,15 +46,18 @@ def wizard():
 
   print "rebooting"
 
-def readBool(text):
-  while True:
-    value = raw_input("%s (yes/no):" % text)
-    if value=="yes":
-      return "1"
-    if value=="no":
-      return "0"
 
-def readInput(text, default=None):
+def readBool(text, default):
+  while True:
+    value = raw_input("%s [%s]:" % (text, "Y/n" if default else "y/N"))
+    if value=="":
+      return (1 if default else 0)
+    if value=="y":
+      return 1
+    if value=="n":
+      return 0
+
+def readInput(text, default):
   value = raw_input("%s [%s]: " % (text, default))
   if value=="":
     value = default
@@ -64,3 +66,11 @@ def readInput(text, default=None):
 def checkResponse(response):
   if response.status_code != requests.codes.ok:
     response.raise_for_status()
+
+def getConfig(keys, default=""):
+  val = config.get("setup")
+  for key in keys.split("."):
+    if val in [default, "", None]:
+      break
+    val = val.get(key, default)
+  return val
